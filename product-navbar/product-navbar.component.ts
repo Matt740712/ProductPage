94827bf0565228d50b07bf2ac89885891796ede9
Matt740712 <mattiatar7@gmail.com>
2024-12-12 15:40:25 +0100

    ComponentProduct

---
+++ product-navbar/product-navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-product-navbar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './product-navbar.component.html',
+  styleUrl: './product-navbar.component.css'
+})
+export class ProductNavbarComponent {
+
+}
